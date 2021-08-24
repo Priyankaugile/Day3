2f017fc42c9c7d79e8511568ec86055ea0a49a01
priyankaugile <ugilepriyanka26@gmail.com>
2021-08-24 22:52:55 +0530

    Address: Code of switch case.

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,26 +1,30 @@
-public class AddPartTimeEmp {
+public class SwitchCase {
+
+    public static final int IS_PART_TIME = 1;
+    public static final int IS_FULL_TIME = 2;
+    public static final int RATE_PER_HR = 20;
 
     public static void main(String[] args) {
-
-        // constants
-        int IS_PART_TIME = 1;
-        int IS_FULL_TIME = 2;
-        int RATE_PER_HR = 20;
-
         //variables
-        int empHr = 0;
-        int empWage =0;
+        int empHrs = 0;
+        int empWage = 0;
 
         //computation
-        double empCheck = Math.floor(Math.random()*10)%3;
-        if (empCheck == IS_PART_TIME)
-            empHr = 4;
-        else if (empCheck == IS_FULL_TIME)
-            empHr = 8;
-        else
-            empHr = 0;
+        int empCheck = (int) Math.floor(Math.random() *10)%3;
 
-        empWage = (empHr * RATE_PER_HR);
+        switch (empCheck){
+            case IS_PART_TIME:
+                empHrs = 4;
+                break;
+            case IS_FULL_TIME:
+                empHrs = 8;
+                break;
+            default:
+                empHrs =0;
+        }
+
+        empWage = empHrs * RATE_PER_HR;
         System.out.println("Emp Wage: " + empWage);
     }
+
 }
