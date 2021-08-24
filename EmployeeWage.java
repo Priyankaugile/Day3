4099a40e51f28db71082b88c035f393dccc585db
priyankaugile <ugilepriyanka26@gmail.com>
2021-08-24 23:07:07 +0530

    Added: Calculating Total Hour,Day and Month .

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,19 +1,21 @@
-public class Cal_Wage_for_month {
+public class Cal_hr_day_for_month {
 
     public static final int IS_PART_TIME = 1;
     public static final int IS_FULL_TIME = 2;
     public static final int RATE_PER_HR = 20;
     public static final int NO_OF_WORKING_DAY = 2;
+    public static final int MAX_HRS_IN_MONTH = 10;
 
     public static void main(String[] args) {
 
         //variables
         int empHrs = 0;
-        int empWage = 0;
-        int totalEmoWage = 0;
+        int totalemphrs = 0;
+        int totalworkingday = 0;
 
         //computation
-        for (int day = 0; day < NO_OF_WORKING_DAY; day++) {
+        while (totalemphrs <= MAX_HRS_IN_MONTH && totalworkingday < NO_OF_WORKING_DAY) {
+            totalworkingday++;
 
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
@@ -28,11 +30,12 @@ public class Cal_Wage_for_month {
                     empHrs = 0;
             }
 
-            empWage = empHrs * RATE_PER_HR;
-            totalEmoWage += empWage;
-            System.out.println("Emp Wage: " + empWage);
+            totalemphrs += empHrs;
+            System.out.println("Day: " + totalworkingday + "Emp hr: " + empHrs);
         }
 
-        System.out.println("Total Emp Wgae: " + totalEmoWage);
+        int totalEmpWage = totalemphrs * RATE_PER_HR;
+        System.out.println("Total Emp Wage: " + totalEmpWage);
+
     }
 }
